794e245960b8e355cd9996585d1ac29b950f5170
CassiaDeny <cassia.deny@gmail.com>
2018-07-14 20:16:20 -0300

    adiciona componente item da lista

--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoListItem from './video-list-item';
 
 const VideoList = (props) => {
+    const videoListItems = props.videos.map(video => {
+        return <VideoListItem key={video.etag} video={video} />
+    });
+
     return (
         <ul className='col-md-4 list-group'>
-            {props.videos.length}
+            {videoListItems}
         </ul>
     )
 };
 
-export default VideoList;
+export default VideoList; 
